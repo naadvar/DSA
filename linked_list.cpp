ecf2e87289d69f2cd4d2ccd3e7853e6e75184c7f
naadvar <47262157+naadvar@users.noreply.github.com>
2019-10-30 23:21:54 -0400

    added_adding_function

--- linked_list.cpp
+++ linked_list.cpp
@@ -1,5 +1,6 @@
 //implementing a simple linked list
 #include<iostream>
+#include <string>
 using namespace std;
 
 struct node{
@@ -35,7 +36,7 @@ class linked_list
                 tail->next = temp;
                 tail = temp;
             }
-            cout<<"Node has been sucessfully created";
+            cout<<"Node has been sucessfully created"<<endl;
 
         }
 
@@ -50,6 +51,38 @@ class linked_list
                 temp = temp ->next;
             }
         }
+        void add_node( string mode,int value)
+        {
+            cout<<"The place you have chosen to add a new node is:"<<mode<<endl;
+            
+            // node *temp = new node;
+            // temp = head;
+            
+            if(head == NULL)
+            {
+                cout<<"The head is null so the node you add right now will be the first"<<endl;
+                creat_node(value);
+            }
+            if(mode == "HEAD")
+            {
+                node * temp = new node;
+                temp->data = value;
+                temp ->next = head;
+                head = temp;
+            }
+            else if(mode == "LAST")
+            {
+                node *new_temp = new node;
+                new_temp -> data = value;
+                new_temp -> next = NULL;
+                tail->next = new_temp;
+                new_temp = tail;
+            }
+            else{
+                cout<<"Wrong mode given"<<endl;
+            }
+        }
+        
 
 };
 
@@ -59,6 +92,8 @@ int main()
     linked.creat_node(1);
     linked.creat_node(2);
     linked.creat_node(3);
+    linked.display();
+    linked.add_node("HEADf",5);
     linked.display();
     return 0;
 }
